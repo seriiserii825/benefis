cdfb15bece3aa17b14ef0441b0bf2d864b5c8a9b
serii <seriiburduja@gmail.com>
2019-08-19 18:40:31 +0300

    connect to ftp

--- front-page.php
+++ front-page.php
@@ -14,34 +14,35 @@
 
 get_header();
 ?>
-<script>
-   jQuery( document ).ready(function() {
-      $('.woocommerce').removeClass();
-   });
-</script>
-<div class="slider">
-    <div class="slider_video_parent">
-	<span></span>
-        <video src="http://benefis.myihor.ru//images/video/10000000_690038651457564_3112166903667890767_n.mp4" class="videoslider" autoplay="" loop="" muted="" width="1280" height="720"></video>
+    <script>
+		jQuery(document).ready(function () {
+			$('.woocommerce').removeClass();
+		});
+    </script>
+    <div class="slider">
+        <div class="slider_video_parent">
+            <span></span>
+            <video src="http://benefis.myihor.ru//images/video/10000000_690038651457564_3112166903667890767_n.mp4"
+                   class="videoslider" autoplay="" loop="" muted="" width="1280" height="720"></video>
+        </div>
     </div>
-</div>
-        <div class="container">
-    <div class="row">
-                <?php get_sidebar(); ?>
-        <?php
-        while ( have_posts() ) :
-            the_post();
+    <div class="container">
+        <div class="row">
+			<?php get_sidebar(); ?>
+			<?php
+			while (have_posts()) :
+				the_post();
 
-            get_template_part( 'template-parts/content', 'productsingle' );
+				get_template_part('template-parts/content', 'productsingle');
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
 
-        endwhile; // End of the loop.
-        ?>
+			endwhile; // End of the loop.
+			?>
+        </div>
     </div>
-</div>
 <?php
 get_footer();
