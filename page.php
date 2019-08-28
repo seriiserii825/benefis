8de842917490def5624ffd0d752e93565dad0e08
serii <seriiburduja@gmail.com>
2019-08-28 19:52:58 +0300

    add tasks

--- page.php
+++ page.php
@@ -14,12 +14,6 @@
 
 	get_header();
 ?>
-    <div class="slider">
-        <div class="slider_video_parent">
-            <span></span>
-            <video src="http://benefis.myihor.ru//images/video/10000000_690038651457564_3112166903667890767_n.mp4" class="videoslider" autoplay="" loop="" muted="" width="1280" height="720"></video>
-        </div>
-    </div>
     <div class="container">
         <div class="row">
             <div class="bs-main">
