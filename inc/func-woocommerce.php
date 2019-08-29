a2b9ca830609262ed19eab2e7bfff64639be282d
serii <seriiburduja@gmail.com>
2019-08-29 19:35:59 +0300

    leibl

--- inc/func-woocommerce.php
+++ inc/func-woocommerce.php
@@ -1,6 +1,6 @@
 <?php
 
-	defined( 'ABSPATH' ) || exit;
+	defined('ABSPATH') || exit;
 
 	function woocommerce_template_loop_product_thumbnail()
 	{
@@ -19,20 +19,21 @@
 	}
 
 	// Get Woocommerce variation price based on product ID
-	function get_variation_price_by_id($product_id, $variation_id){
+	function get_variation_price_by_id($product_id, $variation_id)
+	{
 		$currency_symbol = get_woocommerce_currency_symbol();
 		$product = new WC_Product_Variable($product_id);
 		$variations = $product->get_available_variations();
 		$var_data = [];
 		foreach ($variations as $variation) {
-			if($variation['variation_id'] == $variation_id){
-				$display_regular_price = $variation['display_regular_price'].'<span class="currency">'. $currency_symbol .'</span>';
-				$display_price = $variation['display_price'].'<span class="currency">'. $currency_symbol .'</span>';
+			if ($variation['variation_id'] == $variation_id) {
+				$display_regular_price = $variation['display_regular_price'] . '<span class="currency">' . $currency_symbol . '</span>';
+				$display_price = $variation['display_price'] . '<span class="currency">' . $currency_symbol . '</span>';
 			}
 		}
 
 		//Check if Regular price is equal with Sale price (Display price)
-		if ($display_regular_price == $display_price){
+		if ($display_regular_price == $display_price) {
 			$display_price = false;
 		}
 
@@ -45,11 +46,19 @@
 	}
 
 	add_filter('woocommerce_get_price_html', 'func_change_price', 100, 2);
-	function func_change_price($price, $product){
-//		vardump($product->get_sale_price());
-//		vardump($product->get_regular_price());
-//		vardump(get_woocommerce_currency());
-//		vardump(get_woocommerce_currency_symbol());
-//		vardump(wc_price($product->get_price()));
-////		vardump(get_option('woocommerce_currency'));
+
+	add_filter('woocommerce_sale_flash', 'my_custom_sale_flash', 10, 3);
+	function my_custom_sale_flash($text, $post, $_product)
+	{
+		return ' <span class="onsale">15%</span> ';
+	}
+
+	function func_change_price($price, $product)
+	{
+		//		vardump($product->get_sale_price());
+		//		vardump($product->get_regular_price());
+		//		vardump(get_woocommerce_currency());
+		//		vardump(get_woocommerce_currency_symbol());
+		//		vardump(wc_price($product->get_price()));
+		////		vardump(get_option('woocommerce_currency'));
 	}
